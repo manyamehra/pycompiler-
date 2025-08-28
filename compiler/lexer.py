27666b95077cd242736b74792147cb203e0c6d7a
Manya <manyamehra2002@gmail.com>
2025-08-28 14:14:31 +0200

    changer noms des tokens qui sont en ok avec  regex

--- compiler/lexer.py
+++ compiler/lexer.py
@@ -1,32 +1,32 @@
 
 import re
-from compiler.tokens import KEYWORDS
+from compiler.tokens import Identifiant
 
 TOKEN_SPEC = [
     ('tok_chiffre',     r'\d+'),
     ('tok_identifiant', r'[A-Za-z_][A-Za-z0-9_]*'),
-    ('tok_+', r'\+'),
-    ('tok_-', r'-'),
-    ('tok_*', r'\*'),
-    ('tok_/', r'/'),
-    ('tok_=',r'='),
-    ('tok_(', r'\('),
-    ('tok_)',  r'\)'),
-    ('tok_\n', r'\n'),
+    ('tok_plus', r'\+'),
+    ('tok_minus', r'-'),
+    ('tok_star', r'\*'),
+    ('tok_slash', r'/'),
+    ('tok_egal',r'='),
+    ('tok_lparen', r'\('),
+    ('tok_rparen',  r'\)'),
     ('tok_espace', r'[ \t]+'),   
     ('tok_MISMATCH',   r'.'), 
-    ("tok_{",r'\{'),
-    ( "tok_}",r'\}'),
-    ("tok_[",r'\['),
-    ("tok_]",r'\]'),
-    ("tok_;",r'\;'),
-    ("tok_&",r'\&'),
-    ("tok_&&",r'&&'),
-    ("tok_!=",r'!'),
-    ("tok_||",r'||'),
-    ("tok_==",r'=='),
-    ("tok_!",r'\+'),
-    ("tok_%", r'\+'),  
+    ('tok_NEWLINE', r'\n'),
+    ("tok_lcurly",r'\{'),
+    ( "tok_rcurly",r'\}'),
+    ("tok_lbrack",r'\['),
+    ("tok_rbrack",r'\]'),
+    ("tok_semicolon",r'\;'),
+    ("tok_and",r'\&'),
+    ("tok_AND",r'&&'),
+    ("tok_notequal",r'!'),
+    ("tok_pipe",r'||'),
+    ("tok_equalto",r'=='),
+    ("tok_not",r'\+'),
+    ("tok_pecent", r'\+'),  
 ]
 
 TOKEN_REGEX = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in TOKEN_SPEC)
@@ -44,8 +44,8 @@ def tokenize(code):
             line_num += 1
         elif kind == 'tok_MISMATCH':
             raise SyntaxError(f"Unexpected character '{value}' on line {line_num}")
-        elif kind == 'tok_IDENTIFIER' and value in KEYWORDS:
-            tokens.append(('tok_KEYWORD', value))
+        elif kind == 'tok_identifiant' and value in Identifiant:
+            tokens.append(('tok_identifiant', value))
         else:
             tokens.append((kind, value))
 
