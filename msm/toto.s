046cb437e4307a9f7662edd31d883fb97e0b8cae
Manya <manyamehra2002@gmail.com>
2025-10-25 15:04:34 +0200

    petit

--- msm/toto.s
+++ msm/toto.s
@@ -4,11 +4,11 @@ push 0
 dup
 set 0
 drop 1
-.L0
+.L3
 get 0
 push 5
 cmplt
-jumpf L1
+jumpf L4
 get 0
 send
 get 0
@@ -17,7 +17,8 @@ add
 dup
 set 0
 drop 1
-jump L0
-.L1
+jump L3
+.L4
 drop 1
 halt
+.end
